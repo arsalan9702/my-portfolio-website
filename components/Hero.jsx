72f796d4c3fcf259baed058f34e5527aeeb3d4dd
arsalan <arsalansayed9702@gmail.com>
2025-01-22 23:10:12 +0530

    minor changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
             <div className="flex justify-center relative z-10">
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                     <h1 className="uppercase tracking-widest text-center text-blue-100 max-w-80">
-                        Hello this is portfolio
+                        Welcome to My Portfolio Website
                     </h1>
                 </div>
             </div>
